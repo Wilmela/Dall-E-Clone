5255caa9e2fd75d335fe95f01fd2e8b62bcaf6ed
Wilmela <chukumelawilson@gmail.com>
2023-01-26 08:27:11 +0100

    changed to deployed backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/v1':{
-        target:'http://localhost:8080/api'
-      }
-    }
-  }
-})
+  server: {
+    proxy: {
+      "/v1": {
+        target: "https://dalle-d9wt.onrender.com/api",
+      },
+    },
+  },
+});
